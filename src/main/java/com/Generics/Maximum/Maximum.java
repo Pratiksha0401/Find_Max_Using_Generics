30757fc787274b04f4e94b2435babd99e5d53e24
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-05-05 17:41:25 +0530

    Refactor 2 : create Generic class

--- src/main/java/com/Generics/Maximum/Maximum.java
+++ src/main/java/com/Generics/Maximum/Maximum.java
@@ -1,15 +1,29 @@
 package com.Generics.Maximum;
 
-public class Maximum 
-{
+public class Maximum <E extends Comparable<E>>
+{	
+	E x , y , z;
 
-	public static <E extends Comparable<E>> E printMax(E x , E y , E z) {
+	public Maximum(E x, E y, E z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public  E maximum(){
+		return Maximum.maximum(x, y, z); 
+	}
+
+	public static <E extends Comparable<E>> E maximum(E x , E y , E z) {
 		E max=x;
 		if(y.compareTo(max) > 0 )
 			max=y;
 		if(z.compareTo(max) > 0 )
 			max=z;
+		
 			
 	return max;
 	}
+
+	
 }
--- src/test/java/com/Generics/Maximum/MaximumTest.java
+++ src/test/java/com/Generics/Maximum/MaximumTest.java
@@ -8,65 +8,37 @@ import org.junit.Test;
 public class MaximumTest 
 {
 	@Test
-	public void shouldReturnTrue_WhenMaxIntegerAt1stPosition ()
+	public void shouldReturnTrue_ForMaxInteger ()
 	{
-		 int max=Maximum.printMax(5,1,2);
-		 assertEquals( 5 , max );
+		 int max1=Maximum.maximum(5,1,2);
+		 assertEquals( 5 , max1 );
+		 int max2=Maximum.maximum(1,5,2);
+	     assertEquals( 5 , max2 );
+	     int max3=Maximum.maximum(1,2,5);
+		 assertEquals( 5 , max3 );
 	}
+	    
 	    
 	@Test
-	public void shouldReturnTrue_WhenMaxIntegerAt2ndPosition()
+	public void shouldReturnTrue_ForMaxDouble ()
 	{
-		 int max=Maximum.printMax(1,5,2);
-	     assertEquals( 5 , max );
-	}
-	    
-	@Test
-	public void shouldReturnTrue_WhenMaxIntegerAt3rdPosition()
-	{
-	    int max=Maximum.printMax(1,2,5);
-	    assertEquals( 5 , max );
-	}
-	    
-	@Test
-	public void shouldReturnTrue_WhenMaxDoubleAt1stPosition ()
-	{
-	    double max=Maximum.printMax(5.1,1.3,2.8);
-	    assertEquals( 5.1 , max , 0);
-	}
-	    
-	@Test
-	public void shouldReturnTrue_WhenMaxDoubleAt2ndPosition()
-	{
-		double max=Maximum.printMax(1.3,5.1,2.8);
-		assertEquals( 5.1 , max , 0 );
-	}
-	    
-	 @Test
-	 public void shouldReturnTrue_WhenMaxDoubleAt3rdPosition()
-	{
-		 double max=Maximum.printMax(1.3,2.8,5.1);
-		 assertEquals( 5.1 , max , 0 );
+	    double max1=Maximum.maximum(5.1,1.3,2.8);
+	    assertEquals( 5.1 , max1 , 0);
+	    double max2=Maximum.maximum(1.3,5.1,2.8);
+		assertEquals( 5.1 , max2 , 0 );
+		double max3=Maximum.maximum(1.3,2.8,5.1);
+		 assertEquals( 5.1 , max3 , 0 );
 	}
     
     @Test
     public void shouldReturnTrue_WhenMaxStringAt1stPosition ()
     {
-    	String max=Maximum.printMax("Good","Better","Bad");
-        assertEquals( "Good" , max );
+    	String max1=Maximum.maximum("Good","Better","Bad");
+        assertEquals( "Good" , max1 );
+        String max2=Maximum.maximum("Bad","Good","Better");
+        assertEquals( "Good" , max2 );
+        String max3=Maximum.maximum("Bad","Better","Good");
+        assertEquals( "Good" , max3 );
     }
     
-    @Test
-    public void shouldReturnTrue_WhenMaxStringAt2ndPosition()
-    {
-    	String max=Maximum.printMax("Bad","Good","Better");
-        assertEquals( "Good" , max );
-    }
-    
-    @Test
-    public void shouldReturnTrue_WhenMaxStringAt3rdPosition()
-    {
-    	String max=Maximum.printMax("Bad","Better","Good");
-        assertEquals( "Good" , max );
-    }
 }
